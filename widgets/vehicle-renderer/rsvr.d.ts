bb5b02fdbab39e7f1304721eff2f8f3201673912
Byron Carasco <bcarasco@gmail.com>
2017-10-12 10:51:48 -0700

    VR: Add .d.t.s and jsconfig

---
+++ widgets/vehicle-renderer/rsvr.d.ts
@@ -0,0 +1,15 @@
+declare class RideStylerViewport {
+    /**
+     * Create a new instance of the RideStylerViewport renderer
+     * @param selector A selector designating the container to initialize the viewport in
+     */
+    constructor(selector:string);
+
+    /**
+     * Create a new instance of the RideStylerViewport renderer
+     * @param element The container to initialize the viewport in
+     */
+    constructor(element:Node);
+
+    public Update(instructions:ridestyler.Requests.VehicleRenderInstructions);
+}
