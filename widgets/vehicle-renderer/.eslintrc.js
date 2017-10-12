a159ca693700ae7804d3cf49f2edf97a4907d9c0
Byron Carasco <bcarasco@gmail.com>
2017-10-12 10:11:04 -0700

    VR: Add eslint config

---
+++ widgets/vehicle-renderer/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        ridestyler: false
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "no-console": 0,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
