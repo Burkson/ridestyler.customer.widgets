fadd7f98eb234fd1330d3f98edfa5a93bf41c80d
Byron Carasco <bcarasco@gmail.com>
2017-10-30 06:27:01 -0700

    Showcase: StringHelper: added escapeRegExp
    Added escapeRegExp function in StringHelper for escaping strings before passing them into a regular expression

--- widgets/showcase/src/scripts/helpers/StringHelper.ts
+++ widgets/showcase/src/scripts/helpers/StringHelper.ts
@@ -7,4 +7,17 @@ namespace RideStylerShowcase.StringHelper {
         if (typeof string !== 'string') return undefined;
         return string.trim().substr(0, string.indexOf(' '));
     }
+
+    export function escapeRegExp(string:string):string {
+        /**
+         * Used to match `RegExp`
+         * [syntax characters](http://ecma-international.org/ecma-262/7.0/#sec-patterns).
+         */
+        const reRegExpChar = /[\\^$.*+?()[\]{}|]/g;
+        const reHasRegExpChar = RegExp(reRegExpChar.source);
+
+        return (string && reHasRegExpChar.test(string))
+            ? string.replace(reRegExpChar, '\\$&')
+            : string;
+    }
 }
