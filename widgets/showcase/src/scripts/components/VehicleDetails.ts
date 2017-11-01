18bd5ddbf2cdcaf0b06da250414eea43d2a76abd
Byron Carasco <bcarasco@gmail.com>
2017-11-01 11:03:59 -0700

    Showcase: Add Comments to VehicleDetails

--- widgets/showcase/src/scripts/components/VehicleDetails.ts
+++ widgets/showcase/src/scripts/components/VehicleDetails.ts
@@ -2,37 +2,56 @@ namespace RideStylerShowcase {
     const className = 'ridestyler-showcase-vehicle-details'
 
     export class VehicleDetails extends ComponentBase {
+
+        /**
+         * The vehicle description
+         */
         private vehicleDescriptionElement:HTMLElement;
+        /**
+         * The paint swatch next to the vehicle description
+         */
         private vehiclePaintSwatchElement:HTMLElement;
+        /**
+         * The tire size
+         */
         private tireSizeElement:HTMLElement;
 
         protected initializeComponent() {
             this.component = HTMLHelper.createElementWithClass('div', className + " ridestyler-showcase-details");
             
+            // Title
             HTMLHelper.createElement('div', {
                 className: className + '-title' + " ridestyler-showcase-details-title",
                 text: strings.getString('vehicle-details'),
                 appendTo: this.component
             });
 
+            // Vehicle Description/Paint Swatch Container
             HTMLHelper.createElement('div', {
                 className: className + '-vehicle',
                 append: [
+                    
+                    // Paint Swatch
                     this.vehiclePaintSwatchElement = HTMLHelper.createElement('div', {
                         className: className + '-vehicle-swatch ridestyler-showcase-paint-swatch'
                     }),
+
+                    // Vehicle Description
                     this.vehicleDescriptionElement = HTMLHelper.createElement('div', {
                         className: className + '-vehicle-details'
                     })
+
                 ],
                 appendTo: this.component
             });
 
+            // Tire Size
             this.tireSizeElement = HTMLHelper.createElement('div', {
                 className: className + '-tire-size ridestyler-showcase-details-secondary',
                 appendTo: this.component
             });
 
+            // Change Vehicle Button
             HTMLHelper.createButton({
                 className: className + '-change-vehicle',
                 appendTo: this.component,
@@ -42,7 +61,10 @@ namespace RideStylerShowcase {
                 this.state.changeState(States.ChooseVehicle);
             });
 
+            // Listen for data changes
             this.state.afterDataChange(data => this.onDataChange(data));
+
+            // Switch to paint when clicking on the paint swatch
             this.vehiclePaintSwatchElement.addEventListener('click', () => {
                 if (typeof this.paintSwatchClickCallback === 'function') 
                     this.paintSwatchClickCallback();
