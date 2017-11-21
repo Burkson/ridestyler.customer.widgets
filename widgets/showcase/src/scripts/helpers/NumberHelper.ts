9d9b80d8a5ce1c1f5384e434b2c56f34b95a1741
Byron Carasco <bcarasco@gmail.com>
2017-11-20 16:03:47 -0800

    Showcase: Add NumberHelper.extendRange
    Add a helper function for extending number ranges for use in price calculation

--- widgets/showcase/src/scripts/helpers/NumberHelper.ts
+++ widgets/showcase/src/scripts/helpers/NumberHelper.ts
@@ -8,4 +8,28 @@ namespace RideStylerShowcase.NumberHelper {
     export function bound(number:number, min:number, max:number):number {
         return number < min ? min : (number > max ? max : number);
     }
+
+    /**
+     * Extend a number range with a number
+     * @param number The number to extend the range with
+     * @param range The range to extend
+     * 
+     * @returns The range object that was passed in extended by the number
+     */
+    export function extendRange(number:number, range:Range):Range {
+        if (!range) range = {
+            min: Infinity,
+            max: -Infinity
+        };
+
+        if (number < range.min) range.min = number;
+        if (number > range.max) range.max = number;
+
+        return range;
+    }
+
+    export interface Range {
+        min: number;
+        max: number;
+    }
 }
