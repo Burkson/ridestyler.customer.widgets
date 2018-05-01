9ac91c8cee27789e5df08330d437fb693aaad33a
Tyler <tyoung@burkson.com>
2018-05-01 15:03:05 -0700

    Capitalizes Title

--- widgets/showcase/src/scripts/strings/RideStylerShowcase.strings.en.ts
+++ widgets/showcase/src/scripts/strings/RideStylerShowcase.strings.en.ts
@@ -2,7 +2,7 @@ namespace RideStylerShowcase.strings {
     registerLanguage(LanguageKeys.En, {
         "vehicle-select-visualize-intro": "Visualize wheels, tires and suspension modifications on your vehicle.",
         "vehicle-select-share-intro":     "Share with your friends, or save for inspiration.",
-        "select-your-vehicle":            "Select your Vehicle",
+        "select-your-vehicle":            "Select Your Vehicle",
         "paint":                          "Paint",
         "suspension":                     "Suspension",
         "wheels":                         "Wheels",
