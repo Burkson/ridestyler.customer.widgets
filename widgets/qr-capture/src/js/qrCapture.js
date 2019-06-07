7585f18034579d8fafaa98aca62d63a73f2daade
bbless97 <bbless@burkson.com>
2019-06-06 17:56:04 -0700

    Updated script, minified, and removed functions that were unnecessary.

--- widgets/qr-capture/src/js/qrCapture.js
+++ widgets/qr-capture/src/js/qrCapture.js
@@ -15,65 +15,93 @@
 
 	QrCapture.prototype.StartSession = function() {
 		var self = this;
+		var state;
+		var returnData;
 
-		function start(data){
-			self.SessionStartModel = data;
-			self.CheckSessionStatus(data);
+		self.SendRequest('http://localhost:50199/Session/Start', function(data){
+				if(data != undefined && state == undefined){
+					self.SessionStartModel = data;
+					getSession(data)
+					state = 0;
+				}
+		});
+
+		function checkSessionState(session){
+			if(session != undefined || session.Success == false){
+				switch(session.state){
+					case 1:
+						if(typeof self.OnSessionStart == 'function'){
+							if(state != 1){
+								self.OnSessionStart(session)
+								state = 1;
+							}
+							getSession(session)
+						}
+						break;
+					case 2:
+						if(typeof self.OnWaitingForUpload == 'function'){
+							if(state != 2){
+								self.OnWaitingForUpload(session);
+								state = 2;
+							}
+							getSession(session);
+						}
+						break;
+					case 3:
+						if(typeof self.OnImageReady == 'function'){
+							if(state != 3){
+								session.wheelBoundInformation = parseData(session.wheelBoundInformation);
+								self.OnImageReady(session);
+								state = 3;
+							}
+						}
+						break;
+					case 4:
+						if(typeof self.OnEnded == 'function'){
+							if(state != 4){
+								session.wheelBoundInformation = parseData(session.wheelBoundInformation);
+								self.OnEnded(session);
+								state = 4;
+							}
+						}
+						break;
+				}
+			} else {
+				getSession(returnData)
+			}
 		}
 
-		self.SendRequest('http://localhost:50199/Session/Start', start);
-	}
-
-	QrCapture.prototype.CheckSessionStatus = function(data) {
-		var self = this;
-		var state;
-
-		var myInterval = setInterval(function(){
-			self.SendRequest('http://localhost:50199/Session/Get?id=' + data.session.id, getSession);
-		}, 1500);
-
-		function getSession(returnData){
-			switch(returnData.state){
-				case 1:
-					if(typeof self.OnSessionStart == 'function' && state != 1){
-						self.OnSessionStart(returnData);
-						state = 1;
-					}
-					break;
-				case 2:
-					if(typeof self.OnSessionStart == 'function' && state != 2){
-						self.OnWaitingForUpload(returnData);
-						state = 2;
-					}
-					break;
-				case 3:
-					if(typeof self.OnImageReady == 'function' && state != 3){
-						self.OnImageReady(returnData);
-						clearInterval(myInterval);
-						state = 3;
-					}
-					break;
-				case 4:
-					if(typeof self.OnEnded == 'function' && state != 4){
-						self.OnEnded(returnData);
-						clearInterval(myInterval);
-						state = 4;
-					}
-					break;
+		function getSession(data){
+			if(data.session){
+				self.SendRequest('http://localhost:50199/Session/Get?id=' + data.session.id, checkSessionState);
+			} else if(data.id) {
+				self.SendRequest('http://localhost:50199/Session/Get?id=' + data.id, checkSessionState);
 			}
+		}
+
+		function parseData(data){
+			return JSON.parse(data);
 		}
 	}
 
 	QrCapture.prototype.SendRequest = function(url, callback){
 		var request = new XMLHttpRequest();
 
-		request.onload = function(){
-			var data = JSON.parse(request.response)
-			callback(data);
-		}
+		request.addEventListener('readystatechange', function () {
+			if (this.readyState !== 4) {
+				return;
+			} else {
+				if (this.status === 200) {
+					// Success
+					callback(JSON.parse(this.responseText));
+				} else {
+					// Error
+					callback({ Success: false, Code: 0, Message: this.statusText });
+				}
+			}
+		});
 
 		request.open("GET", url);
-
 		request.send();
 	}
 
