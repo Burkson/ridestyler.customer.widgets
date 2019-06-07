dbf2eb52a174b6a1d29deac27ac73ea8960f5e2d
bbless97 <bbless@burkson.com>
2019-06-07 14:44:10 -0700

    Removed redundant code

--- widgets/qr-capture/dist/qrCapture.js
+++ widgets/qr-capture/dist/qrCapture.js
@@ -16,18 +16,18 @@
 	QrCapture.prototype.StartSession = function() {
 		var self = this;
 		var state;
-		var returnData;
 
-		self.SendRequest('http://localhost:50199/Session/Start', function(data){
-				if(data != undefined && state == undefined){
-					self.SessionStartModel = data;
-					getSession(data)
-					state = 0;
-				}
+		self.SendRequest('Session/Start', function(data){
+				self.SessionStartModel = data;
+				getSession(data.session.id)
+				state = 0;
 		});
 
 		function checkSessionState(session){
-			if(session != undefined || session.Success == false){
+				if(typeof session.wheelBoundInformation === 'string'){
+						session.wheelBoundInformation = parseData(session.wheelBoundInformation);
+				}
+
 				switch(session.state){
 					case 1:
 						if(typeof self.OnSessionStart == 'function'){
@@ -35,7 +35,7 @@
 								self.OnSessionStart(session)
 								state = 1;
 							}
-							getSession(session)
+							getSession(session.id)
 						}
 						break;
 					case 2:
@@ -44,14 +44,13 @@
 								self.OnWaitingForUpload(session);
 								state = 2;
 							}
-							getSession(session);
+							getSession(session.id);
 						}
 						break;
 					case 3:
 						if(typeof self.OnImageReady == 'function'){
 							if(state != 3){
-								session.wheelBoundInformation = parseData(session.wheelBoundInformation);
-								self.OnImageReady(session);
+								self.OnImageReady(session.id);
 								state = 3;
 							}
 						}
@@ -59,33 +58,26 @@
 					case 4:
 						if(typeof self.OnEnded == 'function'){
 							if(state != 4){
-								session.wheelBoundInformation = parseData(session.wheelBoundInformation);
-								self.OnEnded(session);
+								self.OnEnded(session.id);
 								state = 4;
 							}
 						}
 						break;
 				}
-			} else {
-				getSession(returnData)
-			}
 		}
 
-		function getSession(data){
-			if(data.session){
-				self.SendRequest('http://localhost:50199/Session/Get?id=' + data.session.id, checkSessionState);
-			} else if(data.id) {
-				self.SendRequest('http://localhost:50199/Session/Get?id=' + data.id, checkSessionState);
-			}
+		function getSession(sessionId){
+				self.SendRequest('Session/Get?id=' + sessionId, checkSessionState);
 		}
 
 		function parseData(data){
-			return JSON.parse(data);
+				return JSON.parse(data);
 		}
 	}
 
-	QrCapture.prototype.SendRequest = function(url, callback){
+	QrCapture.prototype.SendRequest = function(endpoint, callback){
 		var request = new XMLHttpRequest();
+		var url = 'http://localhost:50199/' + endpoint;
 
 		request.addEventListener('readystatechange', function () {
 			if (this.readyState !== 4) {
@@ -93,10 +85,7 @@
 			} else {
 				if (this.status === 200) {
 					// Success
-					callback(JSON.parse(this.responseText));
-				} else {
-					// Error
-					callback({ Success: false, Code: 0, Message: this.statusText });
+					callback(JSON.parse(this.response));
 				}
 			}
 		});
